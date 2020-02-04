03eb2f95d820094b6a773794af2584f963889bb8
peterbarla <ppepecsek@yahoo.com>
2020-02-04 12:04:39 +0200

    adding script with this reference

---
+++ Ujabb/Assets/Scripts/moveForwardUsingThis.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveForwardUsingThis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private Transform transform;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(1 * Time.deltaTime,0,0);
+    }
+}

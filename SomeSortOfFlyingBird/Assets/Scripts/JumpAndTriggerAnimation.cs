c8f9f4644cdaa679f3fb80dce5acf8c8691bf191
peterbarla <ppepecsek@yahoo.com>
2020-02-18 12:47:10 +0200

    added some touch controller for android paltform and converted it to android

--- SomeSortOfFlyingBird/Assets/Scripts/JumpAndTriggerAnimation.cs
+++ SomeSortOfFlyingBird/Assets/Scripts/JumpAndTriggerAnimation.cs
@@ -5,18 +5,38 @@ public class JumpAndTriggerAnimation : MonoBehaviour
     public Animator animator;
     private Rigidbody2D rb;
     private float JUMP_AMOUNT = 100f;
+    private bool isAndroid = false;
 
     private void Awake()
     {
+        #if UNITY_ANDROID
+                isAndroid = true;
+        #else
+                isAndroid = false;
+        #endif
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-
-        if (Input.GetButtonDown("Space"))
+        if (isAndroid)
         {
-            animator.Play("WhenPressedSpaceMoveNoseUpAnimation");
-            Jump();
+            if(Input.touchCount == 1)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                { 
+                    animator.Play("WhenPressedSpaceMoveNoseUpAnimation");
+                    Jump();
+                }
+            }
+        }
+        else
+        {
+
+            if (Input.GetButtonDown("Space"))
+            {
+                animator.Play("WhenPressedSpaceMoveNoseUpAnimation");
+                Jump();
+            }
         }
     }
 
